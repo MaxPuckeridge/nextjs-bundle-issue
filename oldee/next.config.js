6851d0acb24762e13bcda7160b41be59d008d565
Max Puckeridge <max.puckeridge@gmail.com>
2025-03-19 16:01:04 +1100

    Old

---
+++ oldee/next.config.js
@@ -0,0 +1,7 @@
+const nextConfig = {}
+ 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
